9428f6cf77a58d0189a7b3320ea2f6fe5a544077
nishida takashi <nishida.t.yachiyoroz@gmail.com>
2018-11-21 17:48:18 +0900

    ソート機能完了。ただし、もう少しjsの関数を纏めたいところ

--- application/views/tax2.php
+++ application/views/tax2.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>消費税設定</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo base_url();?>css/styles.css">
   </head>
   <body>
@@ -11,8 +12,8 @@
       <table class="database">
         <thead>
           <tr>
-            <th>開始日付</th>
-            <th>税率</th>
+            <th data-type="start" id="start">開始日付</th>
+            <th data-type="rate" id="rate">税率</th>
           </tr>
         </thead>
         <tbody>
