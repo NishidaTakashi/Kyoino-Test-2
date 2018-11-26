c429f003cd141cdfe1ea16c61cc121d687265e22
nishida takashi <nishida.t.yachiyoroz@gmail.com>
2018-11-26 18:46:27 +0900

    結局jQueryでやる必要がありそう

--- application/views/tax2.php
+++ application/views/tax2.php
@@ -4,11 +4,15 @@
     <meta charset="utf-8">
     <title>消費税設定</title>
     <link rel="stylesheet" href="<?php echo base_url();?>css/styles.css">
+      <script src="<?php echo base_url();?>js/jquery-3.3.1.min.js"></script>
   </head>
   <body>
     <div class="container">
+
       <h3>消費税設定の一覧</h3>
       <table class="database">
+        <?php echo form_open("tax2/update"); ?>
+
         <thead>
           <tr>
             <th>開始日付</th>
@@ -19,6 +23,7 @@
           <?php foreach ($taxes as $tax): ?>
           <tr>
               <td class="start">
+                <!-- <input type="text" name="" value="<?php echo $tax['start'];?>"> -->
               <?php echo $tax["start"];?>
               </td>
               <td class="rate">
@@ -26,7 +31,10 @@
               </td>
           </tr>
         <?php endforeach; ?>
+
         </tbody>
+        <?php echo form_close(); ?>
+
       </table>
     </div>
     <script src="<?php echo base_url();?>js/main.js" charset="utf-8"></script>
---
+++ js/main.js
@@ -0,0 +1,81 @@
+//
+// (function(){
+//   'use strict';
+//   // var tds=document.getElementsByTagName('td')
+//   var start=document.getElementsByClassName("start");
+//   // var rate=document.getElementsByClassName("rate");
+//   var flag=1;//1．編集、-1.更新
+//
+//   // function update(){
+//   //   var i;
+//   //   for (var i = 0; i < tds.length; i++) {
+//   //     tds[i]. addEventListener("click",function(){
+//   //       // tds[i].
+//   //           console.log("start or rate");
+//   //         })
+//   //       }
+//   //     }
+//
+//   function updateStart(){
+//     for (var i = 0; i < start.length; i++) {
+//       start[i]. addEventListener("click",function(){
+//         if (this.getAttribute("id")==="edit") {
+//           console.log("edit");
+//           return;
+//         }
+//         if (flag===-1) {
+//           //DB更新処理
+//           var edit=document.getElementById("edit");
+//           var text=edit.firstChild.value;
+//           console.log(edit.children);
+//           console.log(text);
+//           edit.textContent=text;
+//           edit.removeAttribute("id");
+//           flag *=-1;
+//           console.log(flag);
+//         }else if (flag===1) {
+//           var input = document.createElement("input");
+//           var form = document.createElement("form");
+//           var text=this.textContent;
+//           text=text.trim();
+//           console.log(text);
+//           this.textContent=null;
+//           this.appendChild(input);
+//           input.setAttribute("value",text);
+//           this.setAttribute("id","edit");
+//           console.log(this);
+//           flag *=-1;
+//           console.log(flag);
+//         }
+//       })
+//     }
+//   }
+//
+//
+//   // function updateRate(){
+//   //   var i;
+//   //   for (var i = 0; i < rate.length; i++) {
+//   //     rate[i]. addEventListener("click",function(){
+//   //           console.log("rate");
+//   //         })
+//   //       }
+//   //     }
+//
+//   // update();
+//   updateStart();
+//   // updateRate();
+//
+// })();
+
+jQuery(document).ready(function($){
+  $(".start").on("click",function(){
+    var text=$(this).text();
+    text=text.trim();
+
+    $(this).text("");
+
+    $(this).attr("id","edit");
+    $(this).append("<input type='text'>")
+    console.log(text);
+  })
+})
